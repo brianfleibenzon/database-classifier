d3351d2c360a4fdddb78c467562377faf3365061
Brian Flores Leibenzon <brianfleibenzon@gmail.com>
2020-08-17 18:11:35 -0300

    Add get scan results tests

--- tests/test_database.py
+++ tests/test_database.py
@@ -37,3 +37,21 @@ def test_start_scan_unreachable_database(client, auth):
     database_id = response.get_json().get('_id')
     response = client.post("/api/v1/database/scan/{}".format(database_id), headers=authHeader)
     assert response.status_code == 502
+
+def test_create_and_get_empty_scan(client, auth):
+    authHeader = auth.header()
+    response = client.post("/api/v1/database", data=json.dumps({"host": "test.host", "port": 3306, "username": "test", "password": "test"}), headers=authHeader, content_type='application/json')
+    database_id = response.get_json().get('_id')
+    response = client.get("/api/v1/database/scan/{}".format(database_id), headers=authHeader)
+    assert response.status_code == 200
+    assert response.get_json() == {}
+    response = client.get("/api/v1/database/scan/{}/render".format(database_id), headers=authHeader)
+    assert response.status_code == 200
+
+def test_get_scan_unexisting_databse(client, auth):
+    authHeader = auth.header()
+    response = client.get("/api/v1/database/scan/1", headers=authHeader)
+    assert response.status_code == 404
+    assert response.get_json().get('msg') == 'Database not found'
+    response = client.get("/api/v1/database/scan/1/render", headers=authHeader)
+    assert response.status_code == 404
